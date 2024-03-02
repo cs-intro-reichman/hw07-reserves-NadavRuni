b91aec8d108469d900929d501f91edb268c1b138
NadavRuni <156665035+NadavRuni@users.noreply.github.com>
2024-03-02 11:47:40 +0200

    1

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -7,34 +7,67 @@ public class HashTagTokenizer {
 		String hashTag = args[0];
 		String []dictionary = readDictionary("dictionary.txt");
 		breakHashTag(hashTag, dictionary);
+		//boolean a = existInDictionary(hashTag.substring(1, 2), dictionary);
+		//System.out.println(hashTag.substring(1, 2));
+		//System.out.println(a);
+
+
 	}
 
 	public static String[] readDictionary(String fileName) {
-		String[] dictionary = new String[3000];
-
 		In in = new In(fileName);
-
-		// Your code here
-
+		String[] dictionary = new String[3000];
+		dictionary=in.readAllLines();
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+		boolean a=false;
+		for (int i =0 ; i<3000;i++)
+		{
+			if (dictionary[i].equals(word))
+			{
+				return true;
+			}
+		}
+		return a ; 
+
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
 
 		// Base case: do nothing (return) if hashtag is an empty string.
-        if (hashtag.isEmpty()) {
+        boolean  a;
+		if (hashtag.isEmpty())
+		 {
             return;
         }
- 
-        int N = hashtag.length();
 
-        for (int i = 1; i <= N; i++) {
-		
+		else
+	     {
+			int rem=0;
+            int N = hashtag.length();
+            for (int i = 1; i <= N; i++)
+	     	{
+	    		a=existInDictionary(hashtag.substring(rem, i), dictionary);
+		     	if (a) 
+		    	{
+				    //System.out.println(hashtag.substring(rem, i));
+			     	if(i+1>=N)
+			    	{
+					    return;
+				    }
+			    	else 
+					{
+						System.out.println(hashtag.substring(rem, i));
+						breakHashTag(hashtag.substring(i,N), dictionary);
+					}
+
+		        }
+			
         }
+	}
+
     }
 
 }
--- SpellChecker.java
+++ SpellChecker.java
@@ -3,33 +3,72 @@ public class SpellChecker {
 
 
 	public static void main(String[] args) {
-		String word = args[0];
+		String word1 = args[0];
 		int threshold = Integer.parseInt(args[1]);
 		String[] dictionary = readDictionary("dictionary.txt");
-		String correction = spellChecker(word, threshold, dictionary);
+		String correction = spellChecker(word1, threshold, dictionary);
 		System.out.println(correction);
 	}
 
-	public static String tail(String str) {
-		// Your code goes here
+	public static String tail(String str)
+	 {
+		return str.substring(1);
+	
 	}
 
-	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
+	public static int levenshtein(String word1, String word2) 
+	{
+		int a,b,c;
+		if (word2.length()==0)
+		{
+			return word1.length();
+		}
+		else if (word1.length()==0)
+		{
+			return word2.length();
+		}
+		else if (word1.charAt(0)==word2.charAt(0))
+		{
+			return levenshtein(tail(word1), tail(word2));
+		}
+		else 
+		{ 
+			a=levenshtein(tail(word1), word2);
+			b=levenshtein(word1, tail(word2));
+			c=levenshtein(tail(word1), tail(word2));
+			return 1+Math.min(a, Math.min(c, b));
+
+		}
+
+ 
 	}
 
-	public static String[] readDictionary(String fileName) {
-		String[] dictionary = new String[3000];
-
+	public static String[] readDictionary(String fileName)
+	 {
 		In in = new In(fileName);
-
-		// Your code here
-
+		String[] dictionary = new String[3000];
+		dictionary=in.readAllLines();
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		// Your code goes here
+		int rem=0;
+		int minchange=threshold;
+		int chek;
+		for (int i=0;i<dictionary.length;i++)
+		{
+			chek=levenshtein(word, dictionary[i]);
+			if (chek<minchange)
+			{
+				minchange=chek;
+				rem=i;
+			}
+		}
+		if (minchange==threshold)
+		{
+			return word;
+		}
+		return dictionary[rem];
 	}
 
 }
